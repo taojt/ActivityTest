9df9386d2b7ce5f133b15a055d12a5a7fd617af4
taojt <taojt08@163.com>
2016-07-21 16:08:15 +0800

    Initial commit

---
+++ app/src/main/java/com/example/taojt/activitytest/FirstActivity.java
@@ -0,0 +1,59 @@
+package com.example.taojt.activitytest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch(requestCode){
+            case 1:
+                if(resultCode==RESULT_OK){
+                    String returnData = data.getStringExtra("result_data").toString();
+                    Log.d("FirstActivity", returnData);
+                }
+                break;
+            default:
+
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        Button button = (Button) findViewById(R.id.send_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText(FirstActivity.this, "You click button!",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(FirstActivity.this,SencondActivity.class);
+                String message = "Hello,第二个Activity!";
+                intent.putExtra("extra_data", message);
+                startActivityForResult(intent, 1);
+//                startActivity(intent);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/taojt/activitytest/SencondActivity.java
@@ -0,0 +1,46 @@
+package com.example.taojt.activitytest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SencondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sencond);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        Intent intent = getIntent();
+
+        String message = intent.getStringExtra("extra_data").toString();
+        Toast.makeText(SencondActivity.this, message, Toast.LENGTH_SHORT).show();
+//        Toast.makeText(SencondActivity.this, message, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        intent.putExtra("result_data","Hello, First");
+        setResult(RESULT_OK, intent);
+        finish();
+
+    }
+}
