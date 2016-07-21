311f9527db0f15f7a7d0f29c72a408de55796f67
taojt <taojt08@163.com>
2016-07-21 22:01:57 +0800

    增加menu文件，实现菜单功能，并且修改部分组件

--- app/src/main/java/com/example/taojt/activitytest/FirstActivity.java
+++ app/src/main/java/com/example/taojt/activitytest/FirstActivity.java
@@ -7,6 +7,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -25,6 +27,26 @@ public class FirstActivity extends BasicActivity{
             default:
 
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this,"You clicked Add!",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "You clicked Remove!", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
     }
 
     @Override
